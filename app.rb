2775804ee466022b989e2eaba8232ff3487eaabc
lydzb123 <lydiabaik@gmail.com>
2020-03-24 01:32:31 -0700

    Done.

--- app.rb
+++ app.rb
@@ -6,9 +6,8 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
-
+  post '/results' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
     erb :results
   end
 end
